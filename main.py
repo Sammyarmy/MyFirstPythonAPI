2faca1d69df79c44d58d76c0a346d186314f448b
Sam Armstrong <Sam.Armstrong@sorted.com>
2022-12-07 19:22:17 +0000

    added new user model

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
+from typing import Optional
 from fastapi import Body, FastAPI
+from pydantic import BaseModel
 
 fastapi = FastAPI()
+
+class User(BaseModel):
+    username: str
+    password: str
+    email: Optional[str] = None
+    phone_number: Optional[int] = None
 
 @fastapi.get("/")
 async def root():
     return {"message: Welcome to my API"}
 
 @fastapi.post("/createuser")
-async def create_user(body: dict = Body(...)):
-    print(body)
-    return {"message": "Successfully created user", "username": f"{body['username']}" }
+async def create_user(new_user: User):
+    print(new_user)
+    return {"message": "Successfully created user", "username": f"{new_user.username}", "email": f"{new_user.email}" }
